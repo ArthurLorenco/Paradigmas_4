a0b5f8da79ecae9ba81bae4d81390f27a84ce4b0
HahaAsan <arthurlofreitas06@gmail.com>
2025-06-16 03:00:42 -0300

    classe planos criada

---
+++ Ex9/Planos.java
@@ -0,0 +1,41 @@
+package Ex9;
+
+public class Planos {
+
+    private String nome;
+    private int codigo;
+    private float valorMensal;
+
+    public String getNome() {
+        return nome;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public float getValorMensal() {
+        return valorMensal;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public void setValorMensal(float valorMensal) {
+        this.valorMensal = valorMensal;
+    }
+
+    public Planos(String nome, int codigo, float valorMensal) {
+        this.setNome(nome);
+        this.setCodigo(codigo);
+        this.setValorMensal(Math.round(valorMensal * 100) / 100f);
+    }
+
+    @Override
+    public String toString() {
+        String nomeFormatado = String.format("%-15s", nome);
+        return codigo + "   " + nomeFormatado + "   " + valorMensal;
+    }
+
+}
