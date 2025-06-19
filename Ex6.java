7f8cab41047333f3b8b1fa1f0319a73b2331c81e
Mariana Borges <165071243+MarianaCiervoB@users.noreply.github.com>
2025-06-19 16:56:33 -0300

    Update Ex6.java

--- Ex6.java
+++ Ex6.java
@@ -4,29 +4,25 @@ import java.util.Scanner;
 
 public class AnimaisVistos {
     public static void main(String[] args) {
-        // Criando o HashMap para armazenar os animais e a contagem
         HashMap<String, Integer> animais = new HashMap<>();
 
-        // Dados fictícios da expedição exemplo
-        animais.put("Onça", 3);
+        // exemplos de dados da expedição
+        animais.put("Onça", 2);
         animais.put("Tamanduá", 5);
-        animais.put("Capivara", 8);
-        animais.put("Arara", 12);
+        animais.put("Macaco", 8);
+        animais.put("Bem-te-vi", 12);
         animais.put("Jaguatirica", 2);
 
-        // Leitura do nome do animal a ser consultado
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o nome do animal que deseja verificar: ");
+        System.out.print("Digite o nome do animal que quer buscar: ");
         String animalBuscado = scanner.nextLine();
 
-        // Verificação e exibição do resultado
         if (animais.containsKey(animalBuscado)) {
             int quantidade = animais.get(animalBuscado);
             System.out.printf("%s foi visto %d vez(es) na expedição.%n", animalBuscado, quantidade);
         } else {
             System.out.printf("%s não foi registrado na expedição.%n", animalBuscado);
         }
-
         scanner.close();
     }
 }
