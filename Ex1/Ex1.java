1856cab0d1d3b382d2da066e1049af515966c4e3
Mariana Borges <165071243+MarianaCiervoB@users.noreply.github.com>
2025-06-20 17:24:42 -0300

    Update Ex1.java

--- Ex1/Ex1.java
+++ Ex1/Ex1.java
@@ -1,32 +1,36 @@
+package Ex1;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Ex1 {
     public static void main(String[] args) {
         var l = new ArrayList<String>();
-        l.add("Curitiba" );
+        l.add("Curitiba");
         l.add("Santa Maria");
         l.add("Florianópolis");
         l.add("Manaus");
         l.add("São Paulo");
         l.add("Brasília");
 
-        for (var i = 0; i < l.size();) {
-            int num_de_caracteres = l.get(i).length();
-            if(num_de_caracteres % 2 == 0){
-                System.out.println(l.get(i) + " - Numero de caracteres par: " + num_de_caracteres);
+        Iterator<String> iterator = l.iterator();
+        while (iterator.hasNext()) {
+            String cidade = iterator.next();
+            int num_de_caracteres = cidade.length();
+
+            if (num_de_caracteres % 2 == 0) {
+                System.out.println(cidade + " - Número de caracteres par: " + num_de_caracteres);
                 System.out.println("O dado não será removido.\n");
-                i++;
-            }
-            else {
-                System.out.println(l.get(i) + " - Numero de caracteres impar: " + num_de_caracteres);
+            } else {
+                System.out.println(cidade + " - Número de caracteres ímpar: " + num_de_caracteres);
                 System.out.println("Removendo dado da lista...\n");
-                l.remove(i);
+                iterator.remove();
             }
         }
 
         System.out.println("Resultado final: ");
-        for (var i = 0; i < l.size(); i++) {
-            System.out.println(l.get(i));
+        for (String cidade : l) {
+            System.out.println(cidade);
         }
     }
 }
